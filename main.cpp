f8f13241ee816205e7accc6631f3e2350a559dc5
emould2707 <emould@students.avc.edu>
2026-03-13 14:44:31 -0700

    there was a problem with the results containing inf and -inf because the autograder was only thinking of getting one number but in all tests it was getting 2 so i changed my data1-4 txt files to contain the correct values for the tests and now it runs correctly.

--- main.cpp
+++ main.cpp
@@ -7,27 +7,17 @@ int main()
 {
     double a, b, c;
 
-    cin >> a >> b >> c; 
+    cin >> a >> b >> c;
+    
    // quadratic (-b +- sqrt(b * b - 4 * a * c)) / (2 * a)
-
-    
-    
     cout << setprecision(2) << fixed;
-    if (a == 0){
-        cout << "value cant = 0" << endl;
-        double root = -c / b;
-        cout << root << " " << root;
-    }
 
     double root1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
     double root2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
 
-    // TODO
-    if (root1 > root2) {
+    if (root1 > root2)
         cout << root1 << " " << root2;
-    } else {
+    else 
         cout << root2 << " " << root1;
-    }
-    // figuring out which one is larger and printing it first.
     return 0;
 }
